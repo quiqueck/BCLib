7d3ef6c7563181810127f891884cef47578c0655
Frank <developer@ambertation.de>
2022-07-03 19:52:00 +0200

    Make sure default `level-type` in `server-properties` is betterx

--- src/main/java/org/betterx/worlds/together/mixin/common/DedicatedServerPropertiesMixin.java
+++ src/main/java/org/betterx/worlds/together/mixin/common/DedicatedServerPropertiesMixin.java
@@ -9,6 +9,8 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
+import java.util.Properties;
+
 @Mixin(DedicatedServerProperties.class)
 public class DedicatedServerPropertiesMixin {
     //Make sure the default server properties use our Default World Preset by default (read from "level-type")
@@ -19,5 +21,15 @@ public class DedicatedServerPropertiesMixin {
         }
 
         return string;
+    }
+
+    @ModifyArg(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/dedicated/Settings;<init>(Ljava/util/Properties;)V"))
+    private static Properties wt_defaultPreset(Properties property) {
+        //init default value level preset in server.properties
+        property.setProperty(
+                "level-type",
+                property.getProperty("level-type", WorldPresets.getDEFAULT().location().toString())
+        );
+        return property;
     }
 }
