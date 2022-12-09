c3526376bf44b771ad941ed5dc95c0cae8699b9c
Frank <developer@ambertation.de>
2022-12-09 16:00:22 +0100

    [Changes] Removed old reflection code to create a Disc

--- src/main/java/org/betterx/bclib/items/BaseDiscItem.java
+++ src/main/java/org/betterx/bclib/items/BaseDiscItem.java
@@ -1,12 +1,8 @@
 package org.betterx.bclib.items;
 
-import org.betterx.bclib.BCLib;
-
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.RecordItem;
-
-import java.lang.reflect.Constructor;
 
 public class BaseDiscItem {
     public static RecordItem create(
@@ -15,46 +11,6 @@ public class BaseDiscItem {
             Item.Properties settings,
             int lengthInSeconds
     ) {
-        for (Constructor<?> c : RecordItem.class.getDeclaredConstructors()) {
-            if (c.getParameterCount() == 4) {
-                var types = c.getParameterTypes();
-                if (types.length == 4) { //1.19.1 Constructor
-                    if (
-                            types[0].isAssignableFrom(int.class)
-                                    && types[1].isAssignableFrom(SoundEvent.class)
-                                    && types[2].isAssignableFrom(Item.Properties.class)
-                                    && types[3].isAssignableFrom(int.class)
-                    ) {
-                        c.setAccessible(true);
-                        try {
-                            return (RecordItem) c.newInstance(comparatorOutput, sound, settings, lengthInSeconds);
-                        } catch (Exception e) {
-                            BCLib.LOGGER.error("Failed to instantiate RecordItem", e);
-                        }
-                    }
-                }
-            } else if (c.getParameterCount() == 3) {
-                var types = c.getParameterTypes();
-                if (types.length == 3) { //1.19 constructor
-                    if (
-                            types[0].isAssignableFrom(int.class)
-                                    && types[1].isAssignableFrom(SoundEvent.class)
-                                    && types[2].isAssignableFrom(Item.Properties.class)
-                    ) {
-                        c.setAccessible(true);
-                        try {
-                            return (RecordItem) c.newInstance(comparatorOutput, sound, settings);
-                        } catch (Exception e) {
-                            BCLib.LOGGER.error("Failed to instantiate RecordItem", e);
-                        }
-                    }
-                }
-            }
-        }
-        BCLib.LOGGER.error("No Constructor for RecordItems found:");
-        for (Constructor<?> c : RecordItem.class.getDeclaredConstructors()) {
-            BCLib.LOGGER.error("    - " + c);
-        }
-        return null;
+        return new RecordItem(comparatorOutput, sound, settings, lengthInSeconds);
     }
 }
