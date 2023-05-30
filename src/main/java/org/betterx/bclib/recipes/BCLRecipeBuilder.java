b7739cbedd9977c06bbd0ed7432695a84b5db4fb
Frank <developer@ambertation.de>
2023-05-30 19:27:55 +0200

    [Change] Recipe Builder for recipe that copies smithing templates

--- src/main/java/org/betterx/bclib/recipes/BCLRecipeBuilder.java
+++ src/main/java/org/betterx/bclib/recipes/BCLRecipeBuilder.java
@@ -1,6 +1,10 @@
 package org.betterx.bclib.recipes;
 
+import net.minecraft.data.recipes.RecipeCategory;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.ItemLike;
 
 public class BCLRecipeBuilder {
@@ -18,6 +22,37 @@ public class BCLRecipeBuilder {
 
     public static CraftingRecipeBuilder crafting(ResourceLocation id, ItemLike output) {
         return CraftingRecipeBuilder.make(id, output);
+    }
+
+    private static CraftingRecipeBuilder copySmithingTemplateBase(
+            ResourceLocation id,
+            ItemLike output
+    ) {
+        return CraftingRecipeBuilder
+                .make(id, output)
+                .setOutputCount(2)
+                .setCategory(RecipeCategory.MISC)
+                .addMaterial('#', Items.DIAMOND)
+                .addMaterial('S', output)
+                .setShape("#S#", "#C#", "###");
+    }
+
+    public static CraftingRecipeBuilder copySmithingTemplate(
+            ResourceLocation id,
+            ItemLike output,
+            TagKey<Item> tagKey
+    ) {
+        return copySmithingTemplateBase(id, output)
+                .addMaterial('C', tagKey);
+    }
+
+    public static CraftingRecipeBuilder copySmithingTemplate(
+            ResourceLocation id,
+            ItemLike output,
+            ItemLike ingredient
+    ) {
+        return copySmithingTemplateBase(id, output)
+                .addMaterial('C', ingredient);
     }
 
     public static CookingRecipeBuilder smelting(ResourceLocation id, ItemLike output) {
