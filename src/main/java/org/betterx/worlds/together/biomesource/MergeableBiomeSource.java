4d38b257af80495dbf306b7a6b9fc2b8c96aa4af
Frank <developer@ambertation.de>
2023-06-18 00:32:09 +0200

    [Change] Prevent Crash when merging BiomeSources. A cought crash can result in modded biomes not getting loaded. (quiqueck/BetterNether#138)

--- src/main/java/org/betterx/worlds/together/biomesource/MergeableBiomeSource.java
+++ src/main/java/org/betterx/worlds/together/biomesource/MergeableBiomeSource.java
@@ -1,5 +1,7 @@
 package org.betterx.worlds.together.biomesource;
 
+import org.betterx.bclib.BCLib;
+
 import net.minecraft.core.Holder;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.biome.BiomeSource;
@@ -9,12 +11,19 @@ import java.util.Set;
 public interface MergeableBiomeSource<B extends BiomeSource> {
     default boolean togetherShouldMerge(BiomeSource inputBiomeSource) {
         Set<Holder<Biome>> mySet = ((B) this).possibleBiomes();
-        Set<Holder<Biome>> otherSet = inputBiomeSource.possibleBiomes();
-        if (otherSet.size() != mySet.size()) return true;
+        try {
+            Set<Holder<Biome>> otherSet = inputBiomeSource.possibleBiomes();
 
-        for (Holder<Biome> b : mySet) {
-            if (!otherSet.contains(b))
-                return true;
+            if (otherSet.size() != mySet.size()) return true;
+
+            for (Holder<Biome> b : mySet) {
+                if (!otherSet.contains(b))
+                    return true;
+            }
+        } catch (RuntimeException e) {
+            BCLib.LOGGER.error("Failed to merge BiomeSource", e);
+        } catch (Exception e) {
+            BCLib.LOGGER.error("Failed to merge BiomeSource", e);
         }
 
         return false;
