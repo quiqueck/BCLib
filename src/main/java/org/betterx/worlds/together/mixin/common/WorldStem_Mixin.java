458bc68eefbb2b9d9442fc7d881a49fe5acbd756
Frank <developer@ambertation.de>
2023-06-18 14:45:54 +0200

    [Change] Inject BiomeSource enforcer later than usual to give otehr mods the chance to initialize without having to add a priority (quiqueck/BetterNether#138)

--- src/main/java/org/betterx/worlds/together/mixin/common/WorldStem_Mixin.java
+++ src/main/java/org/betterx/worlds/together/mixin/common/WorldStem_Mixin.java
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
-@Mixin(WorldStem.class)
+@Mixin(value = WorldStem.class, priority = 1500)
 public class WorldStem_Mixin {
     @ModifyVariable(method = "<init>", argsOnly = true, at = @At(value = "INVOKE", target = "Ljava/lang/Record;<init>()V", shift = At.Shift.AFTER))
     LayeredRegistryAccess<RegistryLayer> wt_bake(LayeredRegistryAccess<RegistryLayer> registries) {
