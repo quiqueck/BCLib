b5dc593d9a1b17cc837f23bcc4b9b71c7bf6a72f
Frank <developer@ambertation.de>
2023-06-19 23:10:45 +0200

    [Change] Include Biome count when printing the description of a BiomeSource

--- src/main/java/org/betterx/worlds/together/biomesource/BiomeSourceHelper.java
+++ src/main/java/org/betterx/worlds/together/biomesource/BiomeSourceHelper.java
@@ -7,13 +7,25 @@ import java.util.Collection;
 import java.util.stream.Collectors;
 
 public class BiomeSourceHelper {
+
+    /**
+     * Get a list of namespaces from a collection of biomes.
+     *
+     * @param biomes The collection of biomes.
+     * @return A comma-separated list of namespaces including the number of biomes found in each namespace.
+     */
     public static String getNamespaces(Collection<Holder<Biome>> biomes) {
-        String namespaces = biomes
+        var namespaces = biomes
                 .stream()
                 .filter(h -> h.unwrapKey().isPresent())
                 .map(h -> h.unwrapKey().get().location().getNamespace())
+                .toList();
+
+        return namespaces
+                .stream()
                 .distinct()
+                .map(n -> n + "(" + namespaces.stream().filter(n::equals).count() + ")")
                 .collect(Collectors.joining(", "));
-        return namespaces;
+
     }
 }
