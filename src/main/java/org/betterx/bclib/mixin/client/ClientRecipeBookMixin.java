5b09e484980544a160b33993029929e5d2f2427b
Frank <developer@ambertation.de>
2023-12-18 16:50:43 +0100

    Fixed Recipe Book Mixin

--- src/main/java/org/betterx/bclib/mixin/client/ClientRecipeBookMixin.java
+++ src/main/java/org/betterx/bclib/mixin/client/ClientRecipeBookMixin.java
@@ -4,7 +4,7 @@ import org.betterx.bclib.interfaces.UnknownReceipBookCategory;
 
 import net.minecraft.client.ClientRecipeBook;
 import net.minecraft.client.RecipeBookCategories;
-import net.minecraft.world.item.crafting.Recipe;
+import net.minecraft.world.item.crafting.RecipeHolder;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -14,9 +14,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(ClientRecipeBook.class)
 public abstract class ClientRecipeBookMixin {
     @Inject(method = "getCategory", at = @At("HEAD"), cancellable = true)
-    private static void be_getGroupForRecipe(Recipe<?> recipe, CallbackInfoReturnable<RecipeBookCategories> info) {
-        if (recipe instanceof UnknownReceipBookCategory) {
-            info.setReturnValue(RecipeBookCategories.UNKNOWN);
+    private static void be_getGroupForRecipe(
+            RecipeHolder<?> recipe,
+            CallbackInfoReturnable<RecipeBookCategories> cir
+    ) {
+        if (recipe.value() instanceof UnknownReceipBookCategory) {
+            cir.setReturnValue(RecipeBookCategories.UNKNOWN);
         }
     }
 }
