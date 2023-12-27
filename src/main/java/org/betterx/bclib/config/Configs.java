b3c2b6fd6d79f1378f6b7a90c534c9d8f56d926a
Frank <developer@ambertation.de>
2023-12-27 13:37:47 +0100

    [Feature] Support for Fabric Launcher 15.x (#136)

--- src/main/java/org/betterx/bclib/config/Configs.java
+++ src/main/java/org/betterx/bclib/config/Configs.java
@@ -8,7 +8,6 @@ import net.fabricmc.api.Environment;
 public class Configs {
     // Client and Server-Config must be the first entries. They are not part of the Auto-Sync process
     // But will be needed by other Auto-Sync Config-Files
-    @Environment(EnvType.CLIENT)
     public static final ClientConfig CLIENT_CONFIG = new ClientConfig();
     public static final ServerConfig SERVER_CONFIG = new ServerConfig();
 
