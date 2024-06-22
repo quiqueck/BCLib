fdb1e11de164f94c819a2611eb187ed99f89003a
Frank Bauer <frank.bauer@fau.de>
2024-06-23 01:31:56 +0200

    [Change] Improved ItemStack with nbt codec

--- src/main/java/org/betterx/bclib/util/ItemUtil.java
+++ src/main/java/org/betterx/bclib/util/ItemUtil.java
@@ -6,17 +6,39 @@ import com.mojang.serialization.DataResult;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.core.component.DataComponentMap;
+import net.minecraft.core.component.DataComponents;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.component.ItemAttributeModifiers;
 import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.enchantment.ItemEnchantments;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.function.Function;
 
 public class ItemUtil {
+    private static DataComponentMap getComponents(ItemStack stack) {
+        final DataComponentMap components = stack
+                .getComponents()
+                .filter((c) -> {
+                    if (c == DataComponents.ENCHANTMENTS) {
+                        final ItemEnchantments data = stack.get(DataComponents.ENCHANTMENTS);
+                        if (data != null && !data.isEmpty()) return true;
+                    } else if (c == DataComponents.ATTRIBUTE_MODIFIERS) {
+                        final ItemAttributeModifiers data = stack.get(DataComponents.ATTRIBUTE_MODIFIERS);
+                        if (data != null && !data.modifiers().isEmpty()) return true;
+                    } else if (c == DataComponents.STORED_ENCHANTMENTS) {
+                        final ItemEnchantments data = stack.get(DataComponents.STORED_ENCHANTMENTS);
+                        if (data != null && !data.isEmpty()) return true;
+                    }
+                    return false;
+                });
+        if (components.isEmpty()) return null;
+        return components;
+    }
+
     public static <T> MapCodec<T> codecItemStackWithNBT(
             Function<T, ItemStack> getter,
             Function<ItemStack, T> factory
@@ -27,11 +49,11 @@ public class ItemUtil {
                                       .forGetter(o -> getter.apply(o).getItemHolder()),
                 Codec.INT.optionalFieldOf("count", 1)
                          .forGetter(o -> getter.apply(o).getCount()),
-                DataComponentMap.CODEC.optionalFieldOf("nbt")
-                                      .forGetter(o -> Optional.ofNullable(getter.apply(o).getComponents()))
+                DataComponentMap.CODEC.optionalFieldOf("nbt", null)
+                                      .forGetter(o -> getComponents(getter.apply(o)))
         ).apply(instance, (item, count, nbt) -> {
             var stack = new ItemStack(item, count);
-            if (nbt.isPresent()) stack.applyComponents(nbt.get());
+            if (nbt != null) stack.applyComponents(nbt);
             return factory.apply(stack);
         }));
     }
