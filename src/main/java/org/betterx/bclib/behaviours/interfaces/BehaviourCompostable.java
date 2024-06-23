358ff4faf0b5ab0ed77921ec7d67a4c52aad73b9
Frank Bauer <frank.bauer@fau.de>
2024-06-23 14:06:12 +0200

    Version Bump

--- src/main/java/org/betterx/bclib/behaviours/interfaces/BehaviourCompostable.java
+++ src/main/java/org/betterx/bclib/behaviours/interfaces/BehaviourCompostable.java
@@ -4,7 +4,7 @@ package org.betterx.bclib.behaviours.interfaces;
  * Interface for blocks that can be composted.
  * <p>
  * {@link org.betterx.bclib.api.v2.PostInitAPI} will add the
- * {@link org.betterx.worlds.together.tag.v3.CommonItemTags#COMPOSTABLE} tag to the items of all blocks that
+ * {@link org.betterx.wover.tag.api.predefined.CommonItemTags#COMPOSTABLE} tag to the items of all blocks that
  * implement this interface. It will also register the Block with the {@link org.betterx.bclib.api.v2.ComposterAPI}
  */
 public interface BehaviourCompostable extends BlockBehaviour {
