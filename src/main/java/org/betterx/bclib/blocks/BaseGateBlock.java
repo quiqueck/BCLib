3e13cdeea5e207a3cf6a74e46f4ab886bfc99faf
Frank Bauer <frank.bauer@fau.de>
2024-07-05 00:08:25 +0200

    [Change] Gate Blocks use the DataGenerator to build models/blockstates

--- src/main/java/org/betterx/bclib/blocks/BaseGateBlock.java
+++ src/main/java/org/betterx/bclib/blocks/BaseGateBlock.java
@@ -2,32 +2,31 @@ package org.betterx.bclib.blocks;
 
 import org.betterx.bclib.api.v3.datagen.DropSelfLootProvider;
 import org.betterx.bclib.behaviours.interfaces.BehaviourWood;
-import org.betterx.bclib.client.models.BasePatterns;
-import org.betterx.bclib.client.models.ModelsHelper;
-import org.betterx.bclib.client.models.PatternsHelper;
-import org.betterx.bclib.interfaces.RuntimeBlockModelProvider;
 import org.betterx.wover.block.api.BlockTagProvider;
+import org.betterx.wover.block.api.model.BlockModelProvider;
+import org.betterx.wover.block.api.model.WoverBlockModelGenerators;
 import org.betterx.wover.tag.api.event.context.TagBootstrapContext;
 
-import net.minecraft.client.renderer.block.model.BlockModel;
-import net.minecraft.client.resources.model.ModelResourceLocation;
-import net.minecraft.client.resources.model.UnbakedModel;
-import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.Direction;
+import net.minecraft.data.models.blockstates.MultiVariantGenerator;
+import net.minecraft.data.models.blockstates.PropertyDispatch;
+import net.minecraft.data.models.blockstates.Variant;
+import net.minecraft.data.models.blockstates.VariantProperties;
+import net.minecraft.data.models.model.ModelTemplates;
+import net.minecraft.data.models.model.TextureMapping;
+import net.minecraft.data.models.model.TextureSlot;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.FenceGateBlock;
-import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.WoodType;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 
-import java.util.Map;
-import java.util.Optional;
-import org.jetbrains.annotations.Nullable;
+import java.util.function.BiFunction;
 
-public abstract class BaseGateBlock extends FenceGateBlock implements RuntimeBlockModelProvider, BlockTagProvider, DropSelfLootProvider<BaseGateBlock> {
+public abstract class BaseGateBlock extends FenceGateBlock implements BlockModelProvider, BlockTagProvider, DropSelfLootProvider<BaseGateBlock> {
     private final Block parent;
 
     protected BaseGateBlock(Block source, WoodType type) {
@@ -35,56 +34,69 @@ public abstract class BaseGateBlock extends FenceGateBlock implements RuntimeBlo
         this.parent = source;
     }
 
-
-    @Override
-    @Environment(EnvType.CLIENT)
-    public BlockModel getItemModel(ResourceLocation resourceLocation) {
-        return getBlockModel(resourceLocation, defaultBlockState());
-    }
-
-    @Override
-    @Environment(EnvType.CLIENT)
-    public @Nullable BlockModel getBlockModel(ResourceLocation blockId, BlockState blockState) {
-        boolean inWall = blockState.getValue(IN_WALL);
-        boolean isOpen = blockState.getValue(OPEN);
-        ResourceLocation parentId = BuiltInRegistries.BLOCK.getKey(parent);
-        Optional<String> pattern;
-        if (inWall) {
-            pattern = isOpen
-                    ? PatternsHelper.createJson(
-                    BasePatterns.BLOCK_GATE_OPEN_WALL,
-                    parentId
-            )
-                    : PatternsHelper.createJson(BasePatterns.BLOCK_GATE_CLOSED_WALL, parentId);
-        } else {
-            pattern = isOpen
-                    ? PatternsHelper.createJson(
-                    BasePatterns.BLOCK_GATE_OPEN,
-                    parentId
-            )
-                    : PatternsHelper.createJson(BasePatterns.BLOCK_GATE_CLOSED, parentId);
-        }
-        return ModelsHelper.fromPattern(pattern);
-    }
-
-    @Override
-    @Environment(EnvType.CLIENT)
-    public UnbakedModel getModelVariant(
-            ModelResourceLocation stateId,
-            BlockState blockState,
-            Map<ResourceLocation, UnbakedModel> modelCache
-    ) {
-        boolean inWall = blockState.getValue(IN_WALL);
-        boolean isOpen = blockState.getValue(OPEN);
-        String state = "" + (inWall ? "_wall" : "") + (isOpen ? "_open" : "_closed");
-        ModelResourceLocation modelId = RuntimeBlockModelProvider.remapModelResourceLocation(stateId, blockState, state);
-        registerBlockModel(stateId, modelId, blockState, modelCache);
-        return ModelsHelper.createFacingModel(modelId.id(), blockState.getValue(FACING), true, false);
-    }
-
     @Override
     public void registerBlockTags(ResourceLocation location, TagBootstrapContext<Block> context) {
         context.add(BlockTags.FENCE_GATES, this);
+    }
+
+    @Environment(EnvType.CLIENT)
+    private VariantProperties.Rotation yRotationForState(Direction dir) {
+        VariantProperties.Rotation y = VariantProperties.Rotation.R0;
+        switch (dir) {
+            case EAST:
+                y = VariantProperties.Rotation.R90;
+                break;
+            case NORTH:
+                break;
+            case SOUTH:
+                y = VariantProperties.Rotation.R180;
+                break;
+            case WEST:
+                y = VariantProperties.Rotation.R270;
+                break;
+            default:
+                break;
+        }
+        return y;
+    }
+
+    @Override
+    @Environment(EnvType.CLIENT)
+    public void provideBlockModels(WoverBlockModelGenerators generator) {
+        final var id = TextureMapping.getBlockTexture(parent);
+        final var mapping = new TextureMapping()
+                .put(TextureSlot.TEXTURE, id);
+        final var modelOpen = ModelTemplates.FENCE_GATE_OPEN.createWithSuffix(this, "_open", mapping, generator.modelOutput());
+        final var modelClosed = ModelTemplates.FENCE_GATE_CLOSED.createWithSuffix(this, "_closed", mapping, generator.modelOutput());
+
+        final var modelWallOpen = ModelTemplates.FENCE_GATE_WALL_OPEN.createWithSuffix(this, "_wall_open", mapping, generator.modelOutput());
+        final var modelWallClosed = ModelTemplates.FENCE_GATE_WALL_CLOSED.createWithSuffix(this, "_wall_closed", mapping, generator.modelOutput());
+
+        final BiFunction<Boolean, Boolean, ResourceLocation> modelForState = (inWall, isOpen) -> inWall
+                ? (isOpen ? modelWallOpen : modelWallClosed)
+                : (isOpen ? modelOpen : modelClosed);
+
+        final var props = PropertyDispatch.properties(IN_WALL, OPEN, FACING);
+        final Direction[] directions = {Direction.NORTH, Direction.EAST, Direction.SOUTH, Direction.WEST};
+        final boolean[] open = {true, false};
+
+        for (Direction dir : directions) {
+            for (boolean inWall : open) {
+                for (boolean isOpen : open) {
+                    props.select(inWall, isOpen, dir, Variant
+                            .variant()
+                            .with(VariantProperties.MODEL, modelForState.apply(inWall, isOpen))
+                            .with(VariantProperties.Y_ROT, yRotationForState(dir))
+                    );
+                }
+            }
+        }
+
+        generator.acceptBlockState(MultiVariantGenerator
+                .multiVariant(this)
+                .with(props));
+
+        generator.delegateItemModel(this, modelClosed);
     }
 
     public static class Wood extends BaseGateBlock implements BehaviourWood {
