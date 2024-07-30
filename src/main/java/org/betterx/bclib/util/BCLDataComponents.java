9584ceeaaa3295a8cf932cc92801e7523cacb0b3
Frank Bauer <frank.bauer@fau.de>
2024-07-30 17:59:46 +0200

    [Change] Use changed DataComponent API

--- src/main/java/org/betterx/bclib/util/BCLDataComponents.java
+++ src/main/java/org/betterx/bclib/util/BCLDataComponents.java
@@ -1,7 +1,7 @@
 package org.betterx.bclib.util;
 
 import org.betterx.bclib.BCLib;
-import org.betterx.wover.enchantment.api.EnchantmentManager;
+import org.betterx.wover.data_components.DataComponentManager;
 
 import net.minecraft.core.component.DataComponentType;
 import net.minecraft.world.item.component.CustomData;
@@ -9,7 +9,7 @@ import net.minecraft.world.item.component.CustomData;
 import org.jetbrains.annotations.ApiStatus;
 
 public class BCLDataComponents {
-    public static final DataComponentType<CustomData> ANVIL_ENTITY_DATA = EnchantmentManager.registerDataComponent(
+    public static final DataComponentType<CustomData> ANVIL_ENTITY_DATA = DataComponentManager.registerDataComponent(
             BCLib.makeID("anvil_entity_data"),
             (DataComponentType.Builder<CustomData> builder) -> builder
                     .persistent(CustomData.CODEC)
